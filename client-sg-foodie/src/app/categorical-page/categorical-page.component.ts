12b0bcd9dd10fa9db9563776b53f82be2ca14f57
Quyen <bnbquyen@gmail.com>
2022-06-03 11:07:12 +0700

    2nd push

---
+++ client-sg-foodie/src/app/categorical-page/categorical-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorical-page',
+  templateUrl: './categorical-page.component.html',
+  styleUrls: ['./categorical-page.component.scss'],
+})
+export class CategoricalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
