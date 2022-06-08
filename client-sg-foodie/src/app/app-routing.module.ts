350d8bf3349198b1e24a837d8aa8649ebe88a2f2
nttduonggg <duongntt19406@st.uel.edu.vn>
2022-06-08 18:23:00 +0700

    Add footer and responsive

--- client-sg-foodie/src/app/app-routing.module.ts
+++ client-sg-foodie/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CategoricalPageComponent } from './categorical-page/categorical-page.component';
+import { FooterPage } from './footer/footer.page';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const routes: Routes = [
@@ -9,11 +10,19 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {path:'category/:category',component:CategoricalPageComponent},
+  {path: 'footer', 
+  loadChildren: () => import('./footer/footer.module').then( m => m.FooterPageModule)
+},
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },{path:'navigation-bar',component:NavigationBarComponent},
+  {
+    path: 'footer',
+    loadChildren: () => import('./footer/footer.module').then( m => m.FooterPageModule)
+  },
+
 ];
 
 @NgModule({
