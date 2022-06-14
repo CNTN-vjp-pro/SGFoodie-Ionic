41dcc8708631cb0f5361c7b42ed65d3513762073
nttduonggg <duongntt19406@st.uel.edu.vn>
2022-06-14 12:23:14 +0700

    Add interface Restaurant

---
+++ client-sg-foodie/interfaces/restaurants.ts
@@ -0,0 +1,17 @@
+export interface IRestaurant{
+    name: string,
+    short_description: string,
+    description: [],
+    image: [],
+    menu: [],
+    best_dishes: [],
+    link: string,
+    category: [],
+    address: [],
+    map: string,
+    time: string,
+    price_range: string,
+    special_diet: [],
+    discount: string,
+    cmt: []
+}
