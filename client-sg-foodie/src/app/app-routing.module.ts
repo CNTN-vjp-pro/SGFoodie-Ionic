9beae2961922ae1c06ac1a97305bf7d625bb9247
Quyen <bnbquyen@gmail.com>
2022-06-15 11:13:57 +0700

    Categorical-page final ver

--- client-sg-foodie/src/app/app-routing.module.ts
+++ client-sg-foodie/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { BookingPageModule } from './booking/booking.module';
 const routes: Routes = [
   {
     path: 'home',
