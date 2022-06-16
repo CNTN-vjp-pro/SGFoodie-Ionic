7746ae39ee196a002ffcfdd23c24b0cd0584c84c
NguyenDuyNghia19406c <58812537+NguyenDuyNghia19406c@users.noreply.github.com>
2022-06-16 11:36:55 +0700

    modified user model

--- server-sg-foodie/models/User.js
+++ server-sg-foodie/models/User.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, unique: true, required: true },
+    phoneNumber: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    fullName: { type: String, required: true },
+    dateBirth: { type: Date, required: true },
+    email: { type: String, required: true },
     createdDate: { type: Date, default: Date.now }
 });
 
